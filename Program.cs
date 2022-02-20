c0992d04bef72ade2c00cb75a30a545bba947f80
Tabor Wondimu <csmajortabor@gmail.com>
2022-02-20 09:46:34 -0800

    Created a class that prints

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ClassExercise
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            var carList = new Car();
+            carList.Make = "Lambo";
+            carList.Model = "Urus";
+            carList.Year = 2022;
+            Console.WriteLine($"The make is {carList.Make}, the model is {carList.Model}, it was made in the year {carList.Year} ");
+
+        }
+    }
+} 
